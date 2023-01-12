4a9582d4d309e198ce5581d10baef4110bbbc856
subbotin-n-o <subbotin.n.o@gmail.com>
2023-01-12 23:38:35 +0400

    #3 added package model

---
+++ src/main/java/ru/yandex/practicum/projects_4/model/Courier.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.projects_4.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Courier {
+
+    private String login;
+    private String password;
+    private String firstName;
+    private String id;
+    private String message;
+
+}
---
+++ src/main/java/ru/yandex/practicum/projects_4/model/Couriers.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.projects_4.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Couriers {
+
+    private int id;
+    private String ordersCount;
+}
